3c26cc55e482816b0e12ab2a99b9830b3edc2d42
KanishkSogani <kanishksogani1@gmail.com>
2024-10-24 12:46:02 +0530

    configured appwrite database

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
   appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appwriteAPIKey: String(import.meta.env.VITE_APPWRITE_API_KEY),
   appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
 };
 
 export default conf;
